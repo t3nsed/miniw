466f400f158b2952e7d19f61c1a6b84cdd45523b
Johann Hipp <jhipp@unibz.it>
2020-10-18 13:25:14 +0200

    cleanup

--- src/main/kotlin/parser/MiniArgParser.kt
+++ src/main/kotlin/parser/MiniArgParser.kt
@@ -8,5 +8,5 @@ class MiniArgParser(parser: ArgParser) {
             .default(System.getProperty("java.home"))
     val init by parser.flagging("-i", "--init", help = "setup a new project directory")
     val build by parser.flagging("-b", "--build", help = "build the project")
-    val run by parser.flagging("-r", "--run",  help = "run the project")
+    val run by parser.flagging("-r", "--run", help = "run the project")
 }
