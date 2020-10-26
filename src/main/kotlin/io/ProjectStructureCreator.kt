08d35909ce0184bf96faf640585f2fab84957963
Johann Hipp <jhipp@unibz.it>
2020-10-26 17:15:48 +0100

    formatting

--- src/main/kotlin/io/ProjectStructureCreator.kt
+++ src/main/kotlin/io/ProjectStructureCreator.kt
@@ -5,7 +5,7 @@ import execute.Process
 class ProjectStructureCreator {
     val processor = Process()
 
-    fun init(projectName : String): String {
+    fun init(projectName: String): String {
         supportedOS()
         mkdir(projectName)
         mkdir("$projectName/src")
@@ -14,18 +14,17 @@ class ProjectStructureCreator {
     }
 
     fun supportedOS() {
-        if(processor.createAndReturn("uname") != "linux") {
-            Application.logger.warn() { "Unsupported OS. BSD/MacOS will most certainly still work, but windows won't"}
+        if (processor.createAndReturn("uname") != "linux") {
+            Application.logger.warn() { "Unsupported OS. BSD/MacOS will most certainly still work, but Windows won't" }
         }
     }
 
-
-    private fun mkdir(dirName : String): Boolean {
+    private fun mkdir(dirName: String): Boolean {
         return processor.create("mkdir $dirName")
     }
 
-    private fun touch(fileName : String): Boolean {
-         return processor.create("touch $fileName")
+    private fun touch(fileName: String): Boolean {
+        return processor.create("touch $fileName")
     }
 
     private fun pwd(): String {
