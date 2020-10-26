4eb3c0cb9165114d7a62db2c4ccf757c234ac17a
Johann Hipp <jhipp@unibz.it>
2020-10-26 17:15:28 +0100

    mainstreamed workflow

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,18 +1,40 @@
 import com.xenomachina.argparser.ArgParser
-import com.xenomachina.argparser.mainBody
 import io.ProjectStructureCreator
 import mu.KLogging
 import parser.MiniArgParser
+import validation.Validator
 
 class Application {
     companion object : KLogging()
 
-    fun main(args: Array<String>) = mainBody {
+    fun main(args: Array<String>) {
         logger.info() { "Starting miniw..." }
-        val parsedArgs = ArgParser(args).parseInto(::MiniArgParser).run {
+        runWith(args)
+    }
+
+    private fun runWith(args: Array<String>): Boolean {
+        val config = ArgParser(args).parseInto(::MiniArgParser).run {
             // TODO: if 'init' -> create structure
             // else if 'build' -> fetch dependencies, start java
+            if (!Validator().javaWorks(jdk)) {
+                logger.error() { "Couldn't evaluate where the jdk is; make sure you either have a " +
+                        "valid jdk in \$JAVA_HOME or specified it in the args with --jdk <jdk>" }
+                return false
+            }
+            if (!init.equals("NO_INIT")) {
+                ProjectStructureCreator().init(init)
+                return true
+            }
+            if (run) {
+                // build and run
+                return true
+            }
+            if (build) {
+                // only build
+                return true
+            }
         }
+        return true
     }
 }
 
--- src/main/kotlin/parser/MiniArgParser.kt
+++ src/main/kotlin/parser/MiniArgParser.kt
@@ -4,9 +4,10 @@ import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
 
 class MiniArgParser(parser: ArgParser) {
-    val jdk by parser.flagging("-j", "--jdk", help = "set non-default jdk location")
+    val jdk by parser.storing("-j", "--jdk", help = "set non-default jdk location")
             .default(System.getProperty("java.home"))
-    val init by parser.flagging("-i", "--init", help = "setup a new project directory")
+    val init by parser.storing("-i", "--init", help = "setup a new project directory")
+            .default("NO_INIT")
     val build by parser.flagging("-b", "--build", help = "build the project")
     val run by parser.flagging("-r", "--run", help = "run the project")
 }
